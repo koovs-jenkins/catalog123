52b66a9f2cb50347ad6f20670c83438e248ceb97
Ravi Joshi <rj291991@users.noreply.github.com>
2021-03-18 23:37:03 +0530

    Delete src/context directory

--- src/context/CallCenter/UserCart.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-
-const CallCenterContext = React.createContext({});
-
-export const CallCenterProvider = CallCenterContext.Provider;
-export const CallCenterConsumer = CallCenterContext.Consumer;
